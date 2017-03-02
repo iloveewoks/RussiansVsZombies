ff2a7b541db75eb16f2152635049ec2fd1eb8388
Dmitry Kapitun <kapchook@gmail.com>
2017-03-02 20:05:27 +0300

    Define managers

---
+++ RussiansVsZombies/WaveManager.swift
@@ -0,0 +1,9 @@
+//
+//  WaveManager.swift
+//  RussiansVsZombies
+//
+//  Created by Dmitriy Kapitun on 02/03/17.
+//  Copyright © 2017 Dmitriy Kapitun. All rights reserved.
+//
+
+import Foundation
