1d4432b43b4fb14df659a0b62320548ac63c6c2f
Dmitry Kapitun <kapchook@gmail.com>
2017-03-02 20:19:32 +0300

    Add state machine to game manager

--- RussiansVsZombies/GameManager.swift
+++ RussiansVsZombies/GameManager.swift
@@ -1,9 +1,32 @@
-//
-//  GameManager.swift
-//  RussiansVsZombies
-//
-//  Created by Dmitriy Kapitun on 02/03/17.
-//  Copyright © 2017 Dmitriy Kapitun. All rights reserved.
-//
+import UIKit
 
-import Foundation
+import SpriteKit
+import GameplayKit
+
+class GameManager {
+    
+    var scene: GameScene?
+    
+    var lives = 5
+    var gold = 800
+    var enemiesKilled = 0
+    var waveHasStarted = false
+    
+    static let sharedInstance: GameManager = GameManager()
+    
+    init(){
+        
+    }
+    
+    func set(scene:GameScene){
+        self.scene = scene
+    }
+    
+    // A GameScene state machine
+    lazy var stateMachine: GKStateMachine = GKStateMachine(states: [
+        ReadyState(scene: self.scene!),
+        ActiveState(scene: self.scene!),
+        WinState(scene: self.scene!),
+        LoseState(scene: self.scene!)
+        ])
+}
