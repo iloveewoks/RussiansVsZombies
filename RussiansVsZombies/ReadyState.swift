6592543f79db811aac4e9e79cc76db69e1e30e72
Dmitry Kapitun <kapchook@gmail.com>
2017-03-03 02:03:33 +0300

    Implement states

--- RussiansVsZombies/ReadyState.swift
+++ RussiansVsZombies/ReadyState.swift
@@ -2,5 +2,8 @@ import SpriteKit
 import GameplayKit
 
 class ReadyState: State {
-    
+    override func didEnter(from previousState: GKState?) {
+        scene.playButton.isHidden = false
+        scene.pauseButton.isHidden = true
+    }
 }
