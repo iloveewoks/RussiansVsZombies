0a2245f3d7ec01e79b3dc6de9a37680de71ed58b
Dmitry Kapitun <kapchook@gmail.com>
2017-03-02 20:08:45 +0300

    Add mechanics variables to GameManager

--- RussiansVsZombies/LevelManager.swift
+++ RussiansVsZombies/LevelManager.swift
@@ -7,6 +7,11 @@ class GameManager {
     
     var scene: GameScene?
     
+    var lives = 5
+    var gold = 800
+    var enemiesKilled = 0
+    var waveHasStarted = false
+    
     static let sharedInstance: GameManager = GameManager()
     
     init(){
